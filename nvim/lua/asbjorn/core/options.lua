55b349f9da9258d6ac171cc8dff1bd9bd1bc3890
Asbjørn Lorenzen <asbjornlorenzen@Asbjrns-MacBook-Pro.local>
2024-08-21 12:24:48 +0200

    added nvim configs

---
+++ nvim/lua/asbjorn/core/options.lua
@@ -0,0 +1,34 @@
+--  line numbers
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+-- tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true 
+
+-- line wrapping
+vim.opt.wrap = false
+
+-- searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- appearance
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
+
+-- backspace
+vim.opt.backspace = "indent,eol,start"
+
+-- clipboard
+vim.opt.clipboard:append("unnamedplus")
+
+-- split windows 
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.iskeyword:append("-")
---
+++ nvim/lua/plugins/spec1.lua
@@ -0,0 +1,72 @@
+return {
+	-- the colorscheme should be available when starting Neovim
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {},
+	},
+	{
+		"kyazdani42/nvim-web-devicons",
+		opts = {},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
+	},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	-- snippets
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "L3MON4D3/LuaSnip", dependencies = { "rafamadriz/friendly-snippets" } },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "rafamadriz/friendly-snippets" },
+	-- LSP stuff
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "jose-elias-alvarez/typescript.nvim" },
+	{ "onsails/lspkind.nvim" },
+	-- Linting and formatting
+	{ "jose-elias-alvarez/null-ls.nvim" },
+	{ "jayp0521/mason-null-ls.nvim" },
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	{ "windwp/nvim-autopairs" },
+	{ "windwp/nvim-ts-autotag" },
+	{ "lewis6991/gitsigns.nvim" },
+}
